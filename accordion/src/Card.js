8531c9fbea1040e27c86f0f12d31092d61a1b921
Rani Gupta <rani.gupta259@gmail.com>
2024-02-27 21:24:27 +0530

    fixed the double click bug

--- accordion/src/Card.js
+++ accordion/src/Card.js
@@ -1,16 +1,12 @@
-// import {useState} from "react";
-
-const Card=({id,ques,ans,show,handleShow})=>{
-    // const [show,setShow]=useState(false);
-
-    return <div className="card_container">
+const Card=({id,ques,ans,showItems,handleSetShowIndex})=>{
+  return <div className="card_container">
         <div className="ques_container">
             <h3>{ques}</h3>
-            <button className="toggle_btn" onClick={()=>handleShow()}>
-                    {show?"-":"+"}
+            <button className="toggle_btn" onClick={handleSetShowIndex}>
+                    {showItems?"-":"+"}
             </button>
         </div>
-        {show && <div className="ans_container"><p>{ans}</p></div>}
+        {showItems && <div className="ans_container"><p>{ans}</p></div>}
     </div>
 }
 export default Card;
